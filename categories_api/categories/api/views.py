87d373ff07d9dd2632270080f561dbd1100b4485
nikolay.petrov <niki130396@gmail.com>
2021-11-07 15:42:43 +0200

    Add support for by-depth filtration

--- categories_api/categories/api/views.py
+++ categories_api/categories/api/views.py
@@ -3,18 +3,28 @@ from rest_framework.mixins import (
     DestroyModelMixin,
     ListModelMixin,
     RetrieveModelMixin,
+    UpdateModelMixin,
 )
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from categories_api.categories.models import Category
-from categories_api.categories.api.serializers import CategorySerializer
+from categories_api.categories.api.serializers import CategorySerializer, CategoriesByDepthSerializer
+from rest_framework.decorators import action
+from drf_spectacular.utils import extend_schema
 
 
-class CategoriesViewSet(GenericViewSet, ListModelMixin, CreateModelMixin, DestroyModelMixin, RetrieveModelMixin):
+class CategoriesViewSet(
+    GenericViewSet,
+    ListModelMixin,
+    CreateModelMixin,
+    DestroyModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin
+):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    lookup_field = 'pk'
 
+    @extend_schema(description="Returns the entire category tree")
     def list(self, request, *args, **kwargs):
         root_nodes = Category.objects.all().get_cached_trees()
         data = []
@@ -28,3 +38,15 @@ class CategoriesViewSet(GenericViewSet, ListModelMixin, CreateModelMixin, Destro
         if children:
             result["children"] = children
         return result
+
+    @extend_schema(parameters=[CategoriesByDepthSerializer])
+    @action(detail=False, methods=["GET"], url_path="categories-by-depth")
+    def get_by_depth(self, request, *args, **kwargs):
+        serializer = CategoriesByDepthSerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        from_level = serializer.data["from_level"]
+        nodes_from_level = Category.objects.filter(level=from_level)
+        response = []
+        for node in nodes_from_level:
+            response.append(self.recursive_node_to_dict(node))
+        return Response(response)
